aa041a2bcca4b99195e34d63605096c9ef3f0f6e
Maf38 <mafal.gai@outlook.com>
2019-04-19 11:45:43 +0200

    debut de l'exercice. le test unitaire fonctionne mais j'ai du mal à trouver la BDD crée par entity framework.

---
+++ ChoixResto/Models/Dal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Models
+{
+    public class Dal : IDal
+    {
+        private BddContext bdd;
+        public Dal()
+        {
+              bdd = new BddContext();
+        }
+
+        public List<Resto> ObtientTousLesRestaurants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreerRestaurant(string nom, string telephone)
+
+        {
+            bdd.Restos.Add(new Resto { Nom = nom, Telephone = telephone });
+            bdd.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            bdd.Dispose();
+        }
+
+    }
+}
